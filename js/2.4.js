c2d9271388da692dd6f086bb83ff55b3324dff93
penkin-repo <422000@list.ru>
2022-06-19 21:30:28 +0300

    sec commit

---
+++ js/2.4.js
@@ -0,0 +1,15 @@
+"use strict";
+// 1
+let admin;
+let nameJ = "Джон";
+
+admin = nameJ;
+alert(admin);
+
+//2
+let nameOfOurPlanet = "Earth";
+let nameCurrentUser = "Вася";
+
+//3 Верное
+const BIRTHDAY = '18.04.1982';
+const age = someCode(birthday);
